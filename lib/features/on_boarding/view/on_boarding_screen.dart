c9232380c1eb919218a23bbd061b9e720ebe8e47
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-07-24 14:27:29 +0300

    connect to firebase(android)

--- lib/features/on_boarding/view/on_boarding_screen.dart
+++ lib/features/on_boarding/view/on_boarding_screen.dart
@@ -11,7 +11,6 @@ class OnBoardingScreen extends StatefulWidget {
   State<OnBoardingScreen> createState() => _OnBoardingScreenState();
 }
 
-
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
   final PageController _pageController = PageController();
   int _currentPage = 0;
@@ -50,79 +49,83 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          PageView(
-            controller: _pageController,
-            children: const [
-              OnBoardingWidget(
-                title: "Manage your tasks",
-                subTitle: "You can easily manage all of your daily tasks in DoMe for free",
-                imagePath: 'assets/images/onBoarding1.png',
-              ),
-              OnBoardingWidget(
-                title: "Create daily routine",
-                subTitle: "In Uptodo you can create your personalized routine to stay productive",
-                imagePath: 'assets/images/onBoarding2.png',
-              ),
-              OnBoardingWidget(
-                title: "Organize your tasks",
-                subTitle: "You can organize your daily tasks by adding your tasks into separate categories",
-                imagePath: 'assets/images/onBoarding3.png',
-                buttonText: 'Get Started',
-              ),
-            ],
-          ),
-          Positioned(
-            bottom: 30,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(3, (index) {
-                return Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 4),
-                  width: 8,
-                  height: 8,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _currentPage == index
-                        ? Colors.white
-                        : Colors.grey,
-                  ),
-                );
-              }),
+      body: PopScope(
+        canPop: false,
+        child: Stack(
+          children: [
+            PageView(
+              controller: _pageController,
+              children: const [
+                OnBoardingWidget(
+                  title: "Manage your tasks",
+                  subTitle:
+                      "You can easily manage all of your daily tasks in DoMe for free",
+                  imagePath: 'assets/images/onBoarding1.png',
+                ),
+                OnBoardingWidget(
+                  title: "Create daily routine",
+                  subTitle:
+                      "In Uptodo you can create your personalized routine to stay productive",
+                  imagePath: 'assets/images/onBoarding2.png',
+                ),
+                OnBoardingWidget(
+                  title: "Organize your tasks",
+                  subTitle:
+                      "You can organize your daily tasks by adding your tasks into separate categories",
+                  imagePath: 'assets/images/onBoarding3.png',
+                  buttonText: 'Get Started',
+                ),
+              ],
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            Positioned(
+              bottom: 30,
+              left: 0,
+              right: 0,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomElevatedButton(
-                    text: 'BACK',
-                    onPressed: () {
-                      if (_currentPage > 0) {
-                        _pageController.previousPage(
-                          duration: const Duration(milliseconds: 300),
-                          curve: Curves.easeIn,
-                        );
-                      }
-                    },
-                  ),
-                  CustomElevatedButton(
-                    onPressed: _nextPage,
-                    text: _currentPage == 2 ? 'Get Started' : 'NEXT',
-                  ),
-                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(3, (index) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    width: 8,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentPage == index ? Colors.white : Colors.grey,
+                    ),
+                  );
+                }),
               ),
             ),
-          ),
-        ],
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomElevatedButton(
+                      text: 'BACK',
+                      onPressed: () {
+                        if (_currentPage > 0) {
+                          _pageController.previousPage(
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeIn,
+                          );
+                        }
+                      },
+                    ),
+                    CustomElevatedButton(
+                      onPressed: _nextPage,
+                      text: _currentPage == 2 ? 'Get Started' : 'NEXT',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-       home: SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
