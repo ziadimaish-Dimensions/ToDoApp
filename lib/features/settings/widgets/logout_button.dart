9c3c5551bc3191ca6a1f3b6e6726b5792fa8efca
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-07-28 13:58:17 +0300

    adding logout functionality

--- lib/features/settings/widgets/logout_button.dart
+++ lib/features/settings/widgets/logout_button.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/features/sign_in/view/sign_in_screen.dart';
+import 'package:to_do_app/global/authentication/authentication_repository.dart';
 
 class LogoutButton extends StatelessWidget {
   const LogoutButton({super.key});
+
+  void _handleLogout(BuildContext context) async {
+    final AuthenticationRepository authRepository = AuthenticationRepository();
+    await authRepository.signOut();
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => const SignInScreen()),
+      (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +22,7 @@ class LogoutButton extends StatelessWidget {
       leading: const Icon(Icons.logout, color: Colors.red),
       title: const Text('Log out', style: TextStyle(color: Colors.red)),
       onTap: () {
-        // Handle logout
+        _handleLogout(context);
       },
     );
   }
