0751ab9705df369d004089fcdcc97668164b067a
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-07-28 13:55:51 +0300

    adding loading indicator to sign in and sign up

--- lib/features/sign_up/view/sign_up_screen.dart
+++ lib/features/sign_up/view/sign_up_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/features/home/home_screen.dart';
 import 'package:to_do_app/features/sign_in/view/sign_in_screen.dart';
 import 'package:to_do_app/global/authentication/authentication_repository.dart';
 import 'package:to_do_app/global/authentication/widgets/authentication_widget.dart';
+import 'package:to_do_app/global/widgets/bottom_nav_bar_widget.dart';
 import 'package:to_do_app/global/widgets/dismiss_keyboard.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -17,6 +17,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
   late final TextEditingController emailController;
   late final TextEditingController passwordController;
   final AuthenticationRepository _authRepository = AuthenticationRepository();
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -35,28 +36,42 @@ class _SignUpScreenState extends State<SignUpScreen> {
   }
 
   Future<void> onSignUpPressed() async {
+    setState(() {
+      _isLoading = true;
+    });
+
     final email = emailController.text.trim();
     final password = passwordController.text.trim();
     final confirmPassword = confirmPasswordController.text.trim();
 
     if (email.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
       _showErrorMessage("Please fill in all fields.");
+      setState(() {
+        _isLoading = false;
+      });
       return;
     }
 
     if (password != confirmPassword) {
       _showErrorMessage("Passwords do not match.");
+      setState(() {
+        _isLoading = false;
+      });
       return;
     }
 
     final user = await _authRepository.signUpWithEmailPassword(email, password);
 
     if (mounted) {
+      setState(() {
+        _isLoading = false;
+      });
+
       if (user != null) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => const HomeScreen(),
+            builder: (context) => const BottomNavBarWidget(),
           ),
         );
       } else {
@@ -93,26 +108,37 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DismissKeyboard(
-      child: AuthenticationWidget(
-        title: 'Register',
-        firstTextField: 'Email',
-        secondTextField: 'Password',
-        thirdTextField: 'Confirm Password',
-        actionText: 'Register',
-        bottomText: "Already have an account? Login",
-        isSignUp: true,
-        firstController: emailController,
-        secondController: passwordController,
-        thirdController: confirmPasswordController,
-        onPressed: onSignUpPressed,
-        onGooglePressed: onGoogleLoginPressed,
-        onApplePressed: onAppleLoginPressed,
-        onBottomTextPressed: onBottomTextPressed,
-        firstHintText: 'Enter your Email',
-        secondHintText: 'Enter your Password',
-        thirdHintText: 'Confirm your Password',
-      ),
+    return Stack(
+      children: [
+        DismissKeyboard(
+          child: AuthenticationWidget(
+            title: 'Register',
+            firstTextField: 'Email',
+            secondTextField: 'Password',
+            thirdTextField: 'Confirm Password',
+            actionText: 'Register',
+            bottomText: "Already have an account? Login",
+            isSignUp: true,
+            firstController: emailController,
+            secondController: passwordController,
+            thirdController: confirmPasswordController,
+            onPressed: _isLoading ? () {} : onSignUpPressed,
+            onGooglePressed: onGoogleLoginPressed,
+            onApplePressed: onAppleLoginPressed,
+            onBottomTextPressed: onBottomTextPressed,
+            firstHintText: 'Enter your Email',
+            secondHintText: 'Enter your Password',
+            thirdHintText: 'Confirm your Password',
+          ),
+        ),
+        if (_isLoading)
+          Container(
+            color: Colors.black54,
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+      ],
     );
   }
 }
