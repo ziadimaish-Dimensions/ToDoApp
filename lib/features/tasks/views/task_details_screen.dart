b4ffaaa55732f91d3c5048db68d6710c11e45d9c
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-08-08 04:12:58 +0300

    addimg task details screen

---
+++ lib/features/tasks/views/task_details_screen.dart
@@ -0,0 +1,267 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:to_do_app/global/tasks/task_model.dart';
+import 'package:to_do_app/global/tasks/task_repository.dart';
+import 'package:to_do_app/global/widgets/custom_text_field.dart';
+import 'package:to_do_app/global/widgets/custom_elevated_button.dart';
+
+class TaskDetailScreen extends StatefulWidget {
+  final TaskModel task;
+
+  const TaskDetailScreen({required this.task, super.key});
+
+  @override
+  State<TaskDetailScreen> createState() => _TaskDetailScreenState();
+}
+
+class _TaskDetailScreenState extends State<TaskDetailScreen> {
+  final TaskRepository _taskRepository = TaskRepository();
+  late TaskModel _task;
+
+  @override
+  void initState() {
+    super.initState();
+    _task = widget.task;
+  }
+
+  void _editTask(BuildContext context) {
+    final TextEditingController nameController =
+        TextEditingController(text: _task.name);
+    final TextEditingController detailsController =
+        TextEditingController(text: _task.details);
+    DateTime selectedDate = _task.time;
+    TimeOfDay selectedTime =
+        TimeOfDay(hour: _task.time.hour, minute: _task.time.minute);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey[600],
+          title:
+              const Text('Update Task', style: TextStyle(color: Colors.white)),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomTextField(
+                  controller: nameController,
+                  label: 'Edit Name',
+                  hintText: 'Input the task name',
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: detailsController,
+                  label: 'Edit Details',
+                  hintText: 'Input the task details',
+                ),
+                const SizedBox(height: 10),
+                _DatePicker(
+                  selectedDate: selectedDate,
+                  onDatePicked: (date) {
+                    selectedDate = date;
+                  },
+                ),
+                const SizedBox(height: 10),
+                _TimePicker(
+                  selectedTime: selectedTime,
+                  onTimePicked: (time) {
+                    selectedTime = time;
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                CustomElevatedButton(
+                  onPressed: () {
+                    final updatedTask = TaskModel(
+                      id: _task.id,
+                      name: nameController.text,
+                      details: detailsController.text,
+                      time: DateTime(
+                        selectedDate.year,
+                        selectedDate.month,
+                        selectedDate.day,
+                        selectedTime.hour,
+                        selectedTime.minute,
+                      ),
+                      userId: _task.userId,
+                    );
+                    _taskRepository.updateTask(_task.id, updatedTask);
+                    setState(() {
+                      _task = updatedTask;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  text: 'Update',
+                ),
+                const SizedBox(width: 8),
+                CustomElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  text: 'Cancel',
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteTask(BuildContext context) {
+    _taskRepository.deleteTask(_task.id);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: const Icon(Icons.close, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.edit, color: Colors.white),
+            onPressed: () => _editTask(context),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              _task.name,
+              style: const TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              _task.details,
+              style: const TextStyle(fontSize: 18, color: Colors.white70),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              children: [
+                const Icon(Icons.access_time, color: Colors.white70),
+                const SizedBox(width: 10),
+                Text(
+                  'Task Time: ${DateFormat('yyyy-MM-dd – kk:mm').format(_task.time)}',
+                  style: const TextStyle(color: Colors.white70),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            const Row(
+              children: [
+                Icon(Icons.category, color: Colors.white70),
+                SizedBox(width: 10),
+                Text(
+                  'Task Category: University',
+                  // You can replace this with dynamic category
+                  style: TextStyle(color: Colors.white70),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Spacer(),
+            Center(
+              child: CustomElevatedButton(
+                onPressed: () => _deleteTask(context),
+                text: 'Delete Task',
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DatePicker extends StatelessWidget {
+  final DateTime selectedDate;
+  final Function(DateTime) onDatePicked;
+
+  const _DatePicker({required this.selectedDate, required this.onDatePicked});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            'Picked Date: ${DateFormat('dd-MM-yyyy').format(selectedDate)}',
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        TextButton(
+          onPressed: () async {
+            final DateTime? picked = await showDatePicker(
+              context: context,
+              initialDate: selectedDate,
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2101),
+            );
+            if (picked != null) {
+              onDatePicked(picked);
+            }
+          },
+          child:
+              const Text('Choose Date', style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    );
+  }
+}
+
+class _TimePicker extends StatelessWidget {
+  final TimeOfDay selectedTime;
+  final Function(TimeOfDay) onTimePicked;
+
+  const _TimePicker({
+    required this.selectedTime,
+    required this.onTimePicked,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            'Picked Time: ${selectedTime.format(context)}',
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        TextButton(
+          onPressed: () async {
+            final TimeOfDay? picked = await showTimePicker(
+              context: context,
+              initialTime: selectedTime,
+            );
+            if (picked != null) {
+              onTimePicked(picked);
+            }
+          },
+          child:
+              const Text('Choose Time', style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/tasks/widgets/task_tile_widget.dart
+++ lib/features/tasks/widgets/task_tile_widget.dart
@@ -1,227 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:to_do_app/features/tasks/views/task_details_screen.dart';
 import 'package:to_do_app/global/tasks/task_model.dart';
-import 'package:to_do_app/global/tasks/task_repository.dart';
-import 'package:to_do_app/global/widgets/custom_text_field.dart';
-import 'package:to_do_app/global/widgets/custom_elevated_button.dart';
 
-class TaskTile extends StatefulWidget {
+class TaskTile extends StatelessWidget {
   final TaskModel task;
 
   TaskTile({required this.task, super.key});
 
   @override
-  _TaskTileState createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  final TaskRepository _taskRepository = TaskRepository();
-
-  void _editTask(TaskModel task, BuildContext context) {
-    final TextEditingController nameController =
-        TextEditingController(text: task.name);
-    final TextEditingController detailsController =
-        TextEditingController(text: task.details);
-    DateTime selectedDate = task.time;
-    TimeOfDay selectedTime =
-        TimeOfDay(hour: task.time.hour, minute: task.time.minute);
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: Colors.grey[600],
-          title:
-              const Text('Update Task', style: TextStyle(color: Colors.white)),
-          content: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomTextField(
-                  controller: nameController,
-                  label: 'Edit Name',
-                  hintText: 'Input the task name',
-                ),
-                const SizedBox(height: 10),
-                CustomTextField(
-                  controller: detailsController,
-                  label: 'Edit Details',
-                  hintText: 'Input the task details',
-                ),
-                const SizedBox(height: 10),
-                _DatePicker(
-                  selectedDate: selectedDate,
-                  onDatePicked: (date) {
-                    selectedDate = date;
-                  },
-                ),
-                const SizedBox(height: 10),
-                _TimePicker(
-                  selectedTime: selectedTime,
-                  onTimePicked: (time) {
-                    selectedTime = time;
-                  },
-                ),
-              ],
-            ),
-          ),
-          actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                CustomElevatedButton(
-                  onPressed: () {
-                    final updatedTask = TaskModel(
-                      id: task.id,
-                      name: nameController.text,
-                      details: detailsController.text,
-                      time: DateTime(
-                        selectedDate.year,
-                        selectedDate.month,
-                        selectedDate.day,
-                        selectedTime.hour,
-                        selectedTime.minute,
-                      ),
-                      userId: task.userId,
-                    );
-                    _taskRepository.updateTask(task.id, updatedTask);
-                    Navigator.of(context).pop();
-                  },
-                  text: 'Update',
-                ),
-                const SizedBox(width: 8),
-                CustomElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  text: 'Cancel',
-                ),
-              ],
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[850],
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
             ),
           ],
-        );
-      },
-    );
-  }
-
-  void _viewTask(TaskModel task, BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: Colors.grey[600],
+        ),
+        child: ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
           title: Text(task.name, style: const TextStyle(color: Colors.white)),
-          content:
+          subtitle:
               Text(task.details, style: const TextStyle(color: Colors.white70)),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Close', style: TextStyle(color: Colors.white)),
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TaskDetailScreen(task: task),
             ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Dismissible(
-      key: Key(widget.task.id),
-      background: Container(color: Colors.blue),
-      secondaryBackground: Container(color: Colors.red),
-      onDismissed: (direction) {
-        setState(() {
-          _taskRepository.deleteTask(widget.task.id);
-        });
-      },
-      confirmDismiss: (direction) async {
-        if (direction == DismissDirection.startToEnd) {
-          _editTask(widget.task, context);
-          return false;
-        }
-        return true;
-      },
-      child: ListTile(
-        title:
-            Text(widget.task.name, style: const TextStyle(color: Colors.white)),
-        subtitle: Text(widget.task.details,
-            style: const TextStyle(color: Colors.white70)),
-        onTap: () => _viewTask(widget.task, context),
+          ),
+        ),
       ),
-    );
-  }
-}
-
-class _DatePicker extends StatelessWidget {
-  final DateTime selectedDate;
-  final Function(DateTime) onDatePicked;
-
-  const _DatePicker(
-      {required this.selectedDate, required this.onDatePicked, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Text(
-            'Picked Date: ${DateFormat('dd-MM-yyyy').format(selectedDate)}',
-            style: const TextStyle(color: Colors.white),
-          ),
-        ),
-        TextButton(
-          onPressed: () async {
-            final DateTime? picked = await showDatePicker(
-              context: context,
-              initialDate: selectedDate,
-              firstDate: DateTime(2000),
-              lastDate: DateTime(2101),
-            );
-            if (picked != null) {
-              onDatePicked(picked);
-            }
-          },
-          child:
-              const Text('Choose Date', style: TextStyle(color: Colors.white)),
-        ),
-      ],
-    );
-  }
-}
-
-class _TimePicker extends StatelessWidget {
-  final TimeOfDay selectedTime;
-  final Function(TimeOfDay) onTimePicked;
-
-  const _TimePicker(
-      {required this.selectedTime, required this.onTimePicked, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Text(
-            'Picked Time: ${selectedTime.format(context)}',
-            style: const TextStyle(color: Colors.white),
-          ),
-        ),
-        TextButton(
-          onPressed: () async {
-            final TimeOfDay? picked = await showTimePicker(
-              context: context,
-              initialTime: selectedTime,
-            );
-            if (picked != null) {
-              onTimePicked(picked);
-            }
-          },
-          child:
-              const Text('Choose Time', style: TextStyle(color: Colors.white)),
-        ),
-      ],
     );
   }
 }
